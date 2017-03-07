1a4f614272301bd8bbef40cb05962136b5431e32
Nasir Ayofe <ayofe.nasir@gmail.com>
2017-03-07 02:17:05 -0500

    nothing works

--- JD/src/metrics/RFC.java
+++ JD/src/metrics/RFC.java
@@ -1,8 +1,10 @@
 package metrics;
 
+import java.awt.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,32 +15,64 @@ import ast.SystemObject;
 
 public class RFC 
 {
+	
 	private Map<String, Integer> classMap = new HashMap<String, Integer>();
-	private ArrayList<MethodObject> methods = new ArrayList<MethodObject>();
-	private ArrayList<MethodInvocationObject> invo = new ArrayList<MethodInvocationObject>();
+	private Set<String> M;
+	private Set<String> R;
+	private Set<String> RS;
+	private int RFC;
 	
 	public RFC(SystemObject system)
 	{
-		Set<String> methodSet = new HashSet<String>();
 		Set<ClassObject> classes = system.getClassObjects();
+		int rfcValue;
 		for(ClassObject classObject : classes)
 		{
-			methods = (ArrayList) classObject.getMethodList(); //get all methods in each class
-			
-			for(MethodObject m : methods)
-			{
-				invo = (ArrayList) m.getMethodInvocations();
-				methodSet.add(m.getName());
-			}
-			
-			for(MethodInvocationObject i:invo)
-			{
-				if(!classObject.getName().equals(i.getClass()))
-				methodSet.add(i.getMethodName());
-			}
-			
-			classMap.put(classObject.getName(), methodSet.size());
+			rfcValue = computeRFC(system,classObject);
+			classMap.put(classObject.getName(), rfcValue);
 		}
+	}
+	
+	private int computeRFC(SystemObject system, ClassObject classObject)
+	{
+		M = new HashSet<String>();
+		R = new HashSet<String>();
+		RS = new HashSet<String>();
+		RFC = 0;
+		
+		java.util.List<String> classesNames = system.getClassNames();
+		
+		java.util.List<MethodObject> methods = classObject.getMethodList();
+		
+		for(int i=0; i<methods.size(); i++)
+		{
+			 M.add(methods.get(i).getClassName()+"."+methods.get(i).getSignature()); //methods in the current class
+			 
+			 java.util.List<MethodInvocationObject> methodInvocations = methods.get(i).getMethodInvocations();
+			 
+			 for(int j=0; j<methodInvocations.size(); j++)
+			 {
+				 String originalClassName = methodInvocations.get(j).getOriginClassName();
+				 if(classesNames.contains(originalClassName))
+				 {
+					 R.add(originalClassName+"."+methodInvocations.get(j).getSignature());
+				 }
+			 }
+			
+		}
+		RFC = unionOfSets(M,R);
+		return RFC;
+	}
+	
+	private int unionOfSets(Set set1, Set set2)
+	{
+		Iterator<String> itr = set1.iterator();
+		while(itr.hasNext())
+		{
+			set2.add(itr);
+		}
+		return set2.size();
+		
 	}
 	
 	
--- JD/src/view/MetricsAction.java
+++ JD/src/view/MetricsAction.java
@@ -126,6 +126,7 @@ public class MetricsAction  implements IObjectActionDelegate {
 						System.out.print(cis.toString()+"\n\n");
 						System.out.println("*******************RFC********************");
 						System.out.print(rfc.toString()+"\n\n");
+						System.out.println("*******************RFCh********************");
 						
 						if(selectedPackageFragmentRoot != null) {
 							// package fragment root selected
